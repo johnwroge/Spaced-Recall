b7d52849a82ecc41966dcf3c4934cd00b4ccfc8f
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-07-11 19:56:31 -0400

    p.json

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: 'backend/server.js',
+  entry: './backend/server.js',
   mode: 'development',
   output: {
     path: path.resolve(__dirname, './dist'),
